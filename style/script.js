a906188c14a43e2c44f0af7f184a02e1a5c0a525
Amy Gardiner <amymgardiner@gmail.com>
2022-05-24 15:01:53 -0500

    started over with creating a basic password generator function to then build off of

--- style/script.js
+++ style/script.js
@@ -1,25 +1,11 @@
 // Assignment code here
 
+var chars = "abcdefghijklmnopqrstuvwxyz12345689!@$%^&*"
+var password = ""
 
+function generatePassword() {
 
-
-
-// prompt for length of the password between 8 to 128 characters
-var passwordLength = window.prompt("What is the length of your password?");
-
-// user selects length
-
-
-// prompt for character types
-var passwordLower = window.prompt("Should your password include lowercase characters?");
-var passwordUpper = window.prompt("Should your password include uppercase characters?");
-var passwordNumeric = window.prompt("Should your password include numeric characters?");
-var passwordSpecial = window.prompt("Should your password include special characters?");
-
-// user confirms whether or not to includer lowercase, uppercase, numeric, and/or special characters
-
-
-
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
