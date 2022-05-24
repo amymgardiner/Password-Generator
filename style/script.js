694729a99e1c18e984782dd7110c4ac98f0c7137
Amy Gardiner <amymgardiner@gmail.com>
2022-05-24 15:12:56 -0500

    created a simple password generator function that prints the password on the screen, so far only with lletters

--- style/script.js
+++ style/script.js
@@ -1,9 +1,20 @@
 // Assignment code here
 
-var chars = "abcdefghijklmnopqrstuvwxyz12345689!@$%^&*"
-var password = ""
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_+=/><;:'"
+var newPassword = ""
 
 function generatePassword() {
+  var question = prompt("What is the required length of your password? Pick a number between 8 and 128?", "8");
+  
+  var num = Number(question)
+  console.log(num)
+
+  for (let i = 0; i < num; i++) {
+    var randomNumber = Math.floor(Math.random() * num)
+    newPassword += chars.substring(randomNumber, randomNumber + 1)
+    console.log(newPassword)
+    
+  }
 
 }
 
@@ -15,7 +26,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = newPassword;
 
 }
 
