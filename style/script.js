c2cc2070ba9885e7caed60d6eebdae703aafba60
Amy Gardiner <amymgardiner@gmail.com>
2022-05-24 18:57:18 -0500

    refactor simple password generator, taking char string into multiple arrays to go along with each prompt we'll give the user

--- style/script.js
+++ style/script.js
@@ -1,7 +1,12 @@
 // Assignment code here
-
-var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_+=/><;:'"
+var passwordLength = "";
+var charsArr = [];
 var newPassword = ""
+
+var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var numericArr = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '~'];
 
 function generatePassword() {
   var question = prompt("What is the required length of your password? Pick a number between 8 and 128?", "8");
@@ -11,12 +16,20 @@ function generatePassword() {
 
   for (let i = 0; i < num; i++) {
     var randomNumber = Math.floor(Math.random() * num)
-    newPassword += chars.substring(randomNumber, randomNumber + 1)
+    newPassword = chars.substring (randomNumber, randomNumber + 1)
     console.log(newPassword)
     
   }
 
 }
+
+function userInput() {
+  
+}
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
