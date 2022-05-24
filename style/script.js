18e719b709098c3950592715ed44be19c7dece8a
Amy Gardiner <amymgardiner@gmail.com>
2022-05-23 22:21:03 -0500

    wrote pseudocode for bare outline of what the code needs to do and added the window prompts for the user input

--- style/script.js
+++ style/script.js
@@ -1,6 +1,26 @@
 // Assignment code here
 
 
+
+
+
+// prompt for length of the password between 8 to 128 characters
+window.prompt("What is the length of your password?");
+
+// user selects length
+
+
+// prompt for character types
+window.prompt("Should your password include lowercase characters?");
+window.prompt("Should your password include uppercase characters?");
+window.prompt("Should your password include numeric characters?");
+window.prompt("Should your password include special characters?");
+
+// user confirms whether or not to includer lowercase, uppercase, numeric, and/or special characters
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
